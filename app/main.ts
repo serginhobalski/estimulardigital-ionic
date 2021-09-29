b15098b315e1483aa1178b2128cbc1201718b701
serginhobalski <serginhobalski@gmail.com>
2021-09-28 22:19:47 -0300

    2 files modified

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import './polyfills';
 
+import { initializeApp } from "firebase/app";
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
@@ -13,3 +15,17 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
 
   // Otherise, log the boot error
 }).catch(err => console.error(err));
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB86A2ANK4MZZ70gcxZ0KJKPqVVjtb3chg",
+  authDomain: "my-project-1606234050751.firebaseapp.com",
+  databaseURL: "https://my-project-1606234050751-default-rtdb.firebaseio.com",
+  projectId: "my-project-1606234050751",
+  storageBucket: "my-project-1606234050751.appspot.com",
+  messagingSenderId: "802940266368",
+  appId: "1:802940266368:web:3ba875404b976c8d140365"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
